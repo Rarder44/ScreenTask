cd7b0d1e46c5e788208060dd9c774855491092df
Rarder44 <lpozzi44@gmail.com>
2020-01-17 13:28:36 +0100

    - inizio importazione vecchio codice TCP

--- ScreenTask/Classes/Sender_TCP.cs
+++ ScreenTask/Classes/Sender_TCP.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExtendCSharp.ExtendedClass;
+using ExtendCSharp.TOFIX;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,11 @@ namespace ScreenTask.Classes
 {
     class Sender_TCP :ISenderServices
     {
+
+        List<TcpClientPlus> Clients = new List<TcpClientPlus>();
+        TcpListenerPlus Listener = null;
+
+
         public int SourcePort { get; private set; }
         public int DestinationPort { get; private set; }
         public string SourceIP { get; private set; }
@@ -26,25 +33,80 @@ namespace ScreenTask.Classes
             throw new NotImplementedException();
         }
 
-        public void Send(byte[] data)
+        public async void Send(byte[] data)
         {
+            DataPacket dp = new DataPacket();
+            dp.Data = data;
+            foreach (TcpClientPlus client in Clients.ToArray())
+            {
+                if (!client.Connected)
+                    Client_disconnected(client);
+                else
+                {
+                    try
+                    {
+                        dp.SerializeToStream(client.GetStream()); 
+                    }
+                    catch (Exception ex)
+                    {
+                        //errore nel'invio, client disconnesso -> rimuovo il client 
+                        Client_disconnected(client);
+                    }
+                }
+
+
+                //INVIARE I DATI IN MANIERA ASINCRONA!!!
+            }
             //TODO: scorro tutti i client ed invio il dato 
             //TODO: pensare ad implementare un'ACK del dato con controllo di flusso
         }
 
         public void Start()
         {
-           //TODO: faccio partire il thread di ascolto dei nuovi client
+            //TODO: faccio partire il thread di ascolto dei nuovi client
+            Listener = new TcpListenerPlus(SourceIP, SourcePort);
+            Listener.ClientConnected += Listener_ClientConnected;
+            Listener.Start();
         }
 
         public void Stop()
         {
             //TODO: fermo il thread di ascolto 
+            if (Listener != null)
+                Listener.Stop();
+            foreach (TcpClientPlus client in Clients)
+                client.Close();
+            Clients.Clear();
         }
 
         public void Setup(int? SourcePort, int? DestinationPort, string SourceIP)
         {
-            
+            if (SourcePort == null)
+                this.SourcePort = 7070;
+            else
+                this.SourcePort = SourcePort.Value;
+
+
+            if (DestinationPort == null)
+                this.DestinationPort = 0;
+            else
+                this.DestinationPort = DestinationPort.Value;
+
+
+            if (SourceIP == null)
+                this.SourceIP = "127.0.0.1";
+            else
+                this.SourceIP = SourceIP;
+        }
+
+
+        private void Listener_ClientConnected(TcpClientPlus client)
+        {
+            Clients.Add(client);
+        }
+        private void Client_disconnected(TcpClientPlus client)
+        {
+            Clients.Remove(client);
         }
     }
 }
